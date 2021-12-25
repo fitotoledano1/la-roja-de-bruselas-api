b1c16b628193a248abe5faf593baac8cafe0c241
Fito Toledano <fito.toledano@gmail.com>
2021-12-25 23:37:04 +0100

    added fetch players by position func

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', async (req, res) => {
 app.get('/players', async (req, res) => {
 
     var output = 
-    db.collection("players").get().then((querySnapshot) => {
+    db.collection('players').get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             console.log(doc.id, " => ", doc.data());
             var players = querySnapshot.docs.map(doc => doc.data());
@@ -27,3 +27,18 @@ app.get('/players', async (req, res) => {
     });
     return output;
 });
+
+/// Fetch players by position
+app.get('/:position', async (req, res) => {
+    const position = req.params.position;
+    const query = db.collection('players').where('position', '==', position);
+    const querySnapshot = await query.get();
+
+    if (querySnapshot.size > 0) {
+        var players = querySnapshot.docs.map(doc => doc.data());
+        res.status(200).json(players);
+    }
+    else {
+        res.json({status: 'Not found!'});
+    }
+})
